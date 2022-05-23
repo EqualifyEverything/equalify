d1e752dc7bc6b706031396d6941baf6ce545fea6
Blake Bertuccelli <blakebertuccelli@gmail.com>
2022-05-23 17:26:48 -0500

    a few more enhancements..

--- views/alerts.php
+++ views/alerts.php
@@ -37,22 +37,75 @@
                         </div>
                         <form action="actions/save_alert_filters.php">
                             <div class="modal-body">
-                                <h3 class="h5">Settings</h3>
-                                <div class="mb-3">
-                                    <label for="viewSetting1" class="form-label">View Name</label>
-                                    <input type="text" id="viewSetting2" class="form-control" aria-describedby="metaFilter1Help" value="View One">
-                                </div>
-                                <hr>
                                 <h3 class="h5">Filters</h3>
+                                <div class="row gx-5 mb-3">
+                                    <div class="col">
+                                        <p class="form-label fw-semibold">Integrations</p>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" value="" id="integration_little_forrest">
+                                            <label class="form-check-label" for="integration_little_forrest">
+                                                Little Forrest
+                                            </label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" value="" id="integration_wave">
+                                            <label class="form-check-label" for="integration_wave">
+                                                WAVE
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <p class="form-label fw-semibold">Alert Types</p>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" value="" id="type-error">
+                                            <label class="form-check-label" for="type-error">
+                                                Error
+                                            </label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" value="" id="type-warning">
+                                            <label class="form-check-label" for="type-warning">
+                                                Warning
+                                            </label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" value="" id="type-notice">
+                                            <label class="form-check-label" for="type-notice">
+                                                Notice
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <p class="form-label fw-semibold">Alert Source</p>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" value="" id="source-error">
+                                            <label class="form-check-label" for="source-error">
+                                                Page
+                                            </label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" value="" id="source-system">
+                                            <label class="form-check-label" for="source-system">
+                                                System
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
                                 <div class="mb-3">
-                                    <label for="metaFilter1" class="form-label">Little Forrest Guidelines</label>
+                                    <label for="metaFilter1" class="form-label fw-semibold">Little Forrest Guidelines</label>
                                     <input type="text" id="metaFilter1" class="form-control" aria-describedby="metaFilter1Help">
                                     <div id="metaFilter1Help" class="form-text">All possible guidelines are listed on <a href="https://littleforest.co.uk/">Little Forrest's website</a>. Separate guidelines with commas.</div>
                                 </div>
                                 <div class="mb-3">
-                                    <label for="metaFilter1" class="form-label">Little Forrest Tags</label>
+                                    <label for="metaFilter1" class="form-label fw-semibold">Little Forrest Tags</label>
                                     <input type="text" id="metaFilter1" class="form-control" aria-describedby="metaFilter2Help">
                                     <div id="metaFilter2Help" class="form-text">All possible tags are listed on <a href="https://littleforest.co.uk/">Little Forrest's website</a>. Separate tags with commas.</div>
+                                </div>
+                                <hr>
+                                <h3 class="h5">Settings</h3>
+                                <div class="mb-3">
+                                    <label for="viewSetting1" class="form-label fw-semibold">View Name</label>
+                                    <input type="text" id="viewSetting2" class="form-control" aria-describedby="metaFilter1Help" value="View One">
                                 </div>
                             </div>
                             <div class="modal-footer">
