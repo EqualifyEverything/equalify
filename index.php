3a3fd5d812dae266eda7a06428e4995056482b9c
Blake Bertuccelli <blakebertuccelli@gmail.com>
2022-06-15 12:55:51 -0400

    Brand update.

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ require_once 'actions/install.php';
                 <svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" fill="currentColor" class="bi me-2 bi-patch-check-fill" viewBox="0 0 16 16">
                     <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z"/>
                 </svg>
-                <span class="fs-2">Equalify</span>
+                <span id="brand" class="fs-4 fw-bolder">Equalify</span>
             </a>
             <ul class="nav nav-pills flex-column mb-auto mt-5">
                 <li class="nav-item">
