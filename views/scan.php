e81756a9ea7e006c73ba6077cd73a141a5dbb488
Blake Bertuccelli <blakebertuccelli@gmail.com>
2022-08-01 09:56:52 -0500

    Finalize Scan view.

--- views/scan.php
+++ views/scan.php
@@ -1,4 +1,17 @@
 <section>
-    <h1 class="mb-3 pb-4 border-bottom">Scan</h1>
-    
+    <div class="mb-3 pb-3 border-bottom d-flex justify-content-between align-items-center">
+        <div>
+            <h1>Scan</h1>
+        </div>
+        <div>
+            <a href="#" class="btn btn-primary">
+                Start Scan
+            </a>
+        </div>
+    </div>
+    <div id="terminal" class="bg-dark text-white">
+        <pre><code>
+            No scan running.
+        <pre><code>
+    </div>
 </section>
