2af7a0d199dc9d1d9ab8d8e3cc06d4417092a73c
Blake Bertuccelli <blakebertuccelli@gmail.com>
2022-08-10 14:14:27 -0500

    revert

--- views/alerts.php
+++ views/alerts.php
@@ -40,7 +40,7 @@ if(!empty($_GET['label'])){
             'meta_value' => array(
                 array(
                     'name' => 'title',
-                    'value' => 'All Active Alerts'
+                    'value' => 'All Alerts'
                 ), array(
                     'name' => 'archived',
                     'value' => 0
