5df4f35c4d46e0c0e80d4839fb20c96dc34b5dcd
Blake Bertuccelli <blakebertuccelli@gmail.com>
2022-08-22 14:12:48 -0500

    Minor style fixes.

--- helpers/process_integrations.php
+++ helpers/process_integrations.php
@@ -4,7 +4,7 @@
  * This document runs integrations as they scan each site.
  * 
  * As always, we must remember that every function should 
- * be designed to be as effcient as possible so that 
+ * be designed to be as efficient as possible so that 
  * Equalify works for everyone.
 **********************************************************/
 require_once '../vendor/autoload.php';
