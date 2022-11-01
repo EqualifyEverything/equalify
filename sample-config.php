288f5c85005d58682c2c53180ba70363820f5924
Blake Bertuccelli <blakebertuccelli@gmail.com>
2022-11-01 12:30:10 -0500

    Add ddev as default settings and mention of #40

--- sample-config.php
+++ sample-config.php
@@ -3,29 +3,27 @@
  * These configs are used to setup Equalify's database
  * and execution.
  * 
- * As always, we must remember that every function should 
- * be designed to be as efficient as possible so that 
- * Equalify works for everyone.
+ * By default, configuration that works for ddev is added.
+ * Find out more about setup up Equalify on ddev here:
+ * https://github.com/bbertucc/equalify/issues/40
 **********************************************************/
 
 // Configure database.
-$GLOBALS['DB_HOST'] = 'localhost';
+$GLOBALS['DB_HOST'] = 'ddev-equalify-db';
 $GLOBALS['DB_USERNAME'] = 'root';
 $GLOBALS['DB_PASSWORD'] = 'root';
-$GLOBALS['DB_NAME'] = 'equalify_open_source';
+$GLOBALS['DB_NAME'] = 'equalify';
 $GLOBALS['DB_PORT'] = '3306';
-$GLOBALS['DB_SOCKET'] = '/Applications/MAMP/tmp/mysql/mysql.sock';
+$GLOBALS['DB_SOCKET'] = '/var/run/mysqld/mysqld.sock';
 
 // Configure PHP path, which you can find by running 
-// `which php` in a terminal.
-$GLOBALS['PHP_PATH'] = '/usr/local/bin/php';
+//`which php` in a terminal.
+$GLOBALS['PHP_PATH'] = '/usr/bin/php';
 
-// Scanner settings.
-$GLOBALS['wave_key'] = 'm5WgSFUj2753';
+// Visit https://wave.webaim.org/api/ to get a WAVE key.
+$GLOBALS['wave_key'] = '';
 
 // Additional options.
 $GLOBALS['page_limit'] = '2222';
 $GLOBALS['scan_concurrency'] = '6';
 $GLOBALS['scan_timeout'] = '33';
-$GLOBALS['service_logo'] = 'assets/equalify/equalify-logo.svg';
-$GLOBALS['service_name'] = 'Equalify';
