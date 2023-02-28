b28edd358a7dbc0417f4362eabd47fc06b7bfedb
Blake Bertuccelli <blakebertuccelli@gmail.com>
2023-02-28 15:51:08 -0800

    Cleanup sites

--- views/new_scan_profile.php
+++ views/new_scan_profile.php
@@ -25,39 +25,4 @@
             </button>
         </div>
     </form> 
-
-    <script>
-
-    // Add spinny wheel to button
-    document.getElementById('site_form').addEventListener('submit', function () {
-        document.getElementById('submit').innerHTML = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Adding Content..';
-        document.getElementById("submit").disabled = true;
-    });
-
-    // Add helper text to URL field.
-    function updateHelper(helperText, helperPlaceholder) {
-        document.getElementById('url_helper').innerHTML = helperText;
-        document.getElementById('url').placeholder = helperPlaceholder;
-    }
-    var wordpressHelperText = 'Adds up to 100 WordPress pages.',
-    xmlHelperText = 'URL must be a standard <a href="https://www.sitemaps.org/protocol.html" target="_blank">XML sitemap</a>.';
-    if ( document.getElementById('type').options[document.getElementById('type').selectedIndex].text == 'WordPress Site' ){
-        updateHelper(wordpressHelperText, 'https://equalify.app/')
-    }else if ( document.getElementById('type').options[document.getElementById('type').selectedIndex].text == 'Site via XML Sitemap' ){
-        updateHelper(xmlHelperText, 'http://www.pih.org/sitemap.xml')
-    }else{
-        updateHelper('', 'https://equalify.app/')
-    }
-    document.getElementById('type').addEventListener('change', function () {
-        if ( document.getElementById('type').options[document.getElementById('type').selectedIndex].text == 'WordPress Site' ){
-            updateHelper(wordpressHelperText, 'https://equalify.app/')
-        } else if ( document.getElementById('type').options[document.getElementById('type').selectedIndex].text == 'Site via XML Sitemap' ) {
-            updateHelper(xmlHelperText, 'http://www.pih.org/sitemap.xml')
-        } else {
-            updateHelper('', 'https://equalify.app/')
-        }
-    });
-
-    </script>
-
 </section>
