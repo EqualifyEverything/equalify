bc0dc5abed91ab4d78919db39aaf1f88b9440af7
Blake Bertuccelli <blakebertuccelli@gmail.com>
2023-03-19 14:46:35 -0400

    Update description.

--- index.php
+++ index.php
@@ -57,8 +57,8 @@ if(!empty($_GET['format'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta description="Equalify brings WebOps management into one dashboard with reporting and enforcement tools, integrated with your favorite services." />
-    <title>Equalify | WebOps Management</title>
+    <meta description="Equalify manages web accessibility issues with integrations with your favorite services." />
+    <title>Equalify | Accessibility Issue Management</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link href="theme.css" rel="stylesheet">
 </head>
