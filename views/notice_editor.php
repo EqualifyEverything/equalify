cf84bb9286e3b401851bf3499b3b54e04c383ff7
James Wesley Goedert <jwgoedert@gmail.com>
2023-10-19 16:07:04 -0500

    currently pulling notice with id from notice_id prop in URL, needs meta data and properties linked to table with ids

--- views/notice_editor.php
+++ views/notice_editor.php
@@ -1,21 +1,24 @@
 <?php
 //Setting up alert editor variables//
 $viewTitle = "Notice Editor";
-$message = "This is a sample message";
+$message = "Please enter message";
 $status = 'Status';
 $statusArray = [
   'Equalified',
   'Active',
   'Ignored',
 ];
-$relatedURL = 'www.sampleurl.com/example';
-$property = 'Sample Property';
-$propertiesArray = [
+$currentNotice = '';
+$existing_notices = array();
+$relatedURL = 'Please enter related URL';
+$property = 'Enter Property';
+$propertiesArrayDummy = [
   'sampleProperty1',
   'sampleProperty2',
   'sampleProperty3',
   'sampleProperty4',
 ];
+$propertiesArray = array();
 $snippet = '<h1>This is a sample code snippet,/h1>';
 $snippetArray = [
   '<h1>This is a sample code snippet,</h1>',
@@ -27,6 +30,107 @@ $moreInfoURL = 'www.moreinfo.com/example';
 $notes = 'this is a sample note, with markdown';
 $hasBulkImporter = false;
 $hasVisualizer = false;
+?>
+<?php
+// We use this view to customize reports if a id is 
+// provided, otherwise we create a new report.
+if (!empty($_GET['notice_id'])) {
+  // Set the meta name
+  $notice_id = $_GET['notice_id'];
+  $existing_notices = DataAccess::get_db_rows(
+    'notices',
+    [],
+    1,
+    1000000
+  )['content'];
+
+  // Let's find the item in the array with the matching id
+  foreach ($existing_notices as $notice) {
+    if ($notice->id == $notice_id) {
+      $currentNotice = $notice;
+      $message = $currentNotice->message;
+      break; // Exit the loop once a match is found
+    }
+  }
+
+  // Check if a match was found
+  if (isset($currentNotice)) {
+    // Access the found item
+    echo $currentNotice->message;
+    $noticeId = $currentNotice->id;
+    $message = $currentNotice->message;
+    $status = $currentNotice->status;
+    $relatedURL = $currentNotice->related_url;
+    $property = $currentNotice->property_id;
+    //!!!These variables will be included under meta when updated!!!
+    // $snippetArray = $currentNotice->meta->snippets;
+    // $moreInfoURL = $currentNotice->meta->more_info_url;
+    // $notes = $currentNotice->meta->notes;
+
+    // Use $itemId and $itemName as needed
+    echo "Item ID: $noticeId, Item Message: $message";
+  } else {
+    echo "Item with ID $notice_id not found.";
+  }
+  // Some reports are preset. Presets have restricted fields
+  // you can edit and special naming rules.
+  // $presets = array(
+  //     'report_equalified', 'report_ignored', 'report_all',
+  //     'report_active', 'report_active'
+  // );
+  // if(in_array($name, $presets))
+  //     $preset = TRUE;
+
+  // Let's load in predefined variables for the report.
+
+  // Some reports, like Equalified notices, won't
+  // have data, so we'll have to prepare variables
+  if (empty($existing_notices)) {
+
+    // Set the default title field.
+    if ($name == 'report_equalified') {
+      $title = 'Equalified Notices';
+    } elseif ($name == 'report_ignored') {
+      $title = 'Ignored Notices';
+    } elseif ($name == 'report_all') {
+      $title = 'All Notices';
+    } elseif ($name == 'report_active') {
+      $title = 'Active Notices';
+    }
+
+    // Set the default status field.
+    if ($name == 'report_equalified') {
+      $status = 'equalified';
+    } elseif ($name == 'report_ignored') {
+      $status = 'ignored';
+    } elseif ($name == 'report_active') {
+      $status = 'active';
+    }
+  }
+
+  // }else{
+
+  //     // Let's reformat the meta so we can use it in a
+  //     // more understandable format. The dynamically
+  //     // added content is added to $dynamic_meta.
+  //     $dynamic_meta = array();
+  //     foreach($existing_report as $report) {
+  //         if($report['name'] == 'title'){
+  //             $title = $report['value'];
+  //         }elseif($report['name'] == 'type'){
+  //             $type = $report['value'];
+  //         }elseif($report['name'] == 'status'){
+  //             $status = $report['value'];
+  //         }elseif($report['name'] == 'property_id'){
+  //             $property_id = $report['value'];
+  //         }else{
+  //             $dynamic_meta[] = $report['name'];
+  //         }
+  //     }
+
+  // }
+
+}
 ?>
 
 <main class="container">
@@ -49,7 +153,7 @@ $hasVisualizer = false;
       <div class="row mb-3">
         <div class="col-md-5 mb-4">
           <label class="col-form-label-lg font-weight-bold" for="alertMessageInput">Message</label>
-          <input type="text" class="form-control" id="alertMessageInput" placeholder="Message" value=<?php echo $message ?> required>
+          <input type="text" class="form-control" id="alertMessageInput" placeholder="Message" value="<?php echo $message ?>" required>
         </div>
         <div class="col-md-3 ml-3 mb-4">
           <label class="col-form-label-lg font-weight-bold" for="alertStatusSelectInput">Status</label>
@@ -64,7 +168,7 @@ $hasVisualizer = false;
         </div>
         <div class="col-md-3 ml-3 mb-4">
           <label class="col-form-label-lg font-weight-bold" for="relatedURLInput">Related URL</label>
-          <input type="url" class="form-control" id="relatedURLInput" placeholder="Related URL:" value=<?php echo $relatedURL ?> required>
+          <input type="url" class="form-control" id="relatedURLInput" placeholder="Related URL:" value="<?php echo $relatedURL ?>" required>
         </div>
       </div>
       <!-- Second row -->
@@ -84,9 +188,9 @@ $hasVisualizer = false;
           <ul class="list-group mb-3">
             <?php foreach ($snippetArray as $snippet) : ?>
               <li class="list-group-item d-flex align-items-start">
-                
-                <?php echo '<code  id="metaCodeSnippetInput">'.htmlspecialchars($snippet).'</code>'?>
-                
+
+                <?php echo '<code  id="metaCodeSnippetInput">' . htmlspecialchars($snippet) . '</code>' ?>
+
                 <a href="#" class="ml-3 justify-content-end">
                   <svg xmlns="http://www.w3.org/2000/svg" height="1.25em" viewbox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
                     <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
@@ -105,7 +209,7 @@ $hasVisualizer = false;
         <div class="col-md-3 ml-md-3 mb-4">
           <label class="col-form-label-lg font-weight-bold" for="moreInfoURLInput">Meta: More Info URL</label>
 
-          <input type="url" class="form-control" id="moreInfoURLInput" placeholder="More Info URL:" value=<?php echo $moreInfoURL ?> required>
+          <input type="url" class="form-control" id="moreInfoURLInput" placeholder="More Info URL:" value="<?php echo $moreInfoURL ?>" required>
         </div>
       </div>
       <!-- Third row -->
