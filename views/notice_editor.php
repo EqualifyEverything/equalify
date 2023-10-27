65eae439172683adb1eae6114feaac1bc24c6eec
James Wesley Goedert <jwgoedert@gmail.com>
2023-10-27 12:50:09 -0500

    Add snippet button now working, need to populate from existing snippets

--- views/notice_editor.php
+++ views/notice_editor.php
@@ -46,7 +46,7 @@ if (!empty($_GET['notice_id'])) {
   // Let's find the item in the array with the matching id
   foreach ($existing_notices as $notice) {
     if ($notice->id == $_SESSION['notice_id']) {
-      $currentNotice = $notice; 
+      $currentNotice = $notice;
       break; // Exit the loop once a match is found
     }
   }
@@ -84,10 +84,9 @@ if (!empty(get_object_vars($currentNotice))) {
 
 <title><?php echo $viewTitle ?></title>
 <div class="container mt-5">
-  <div class="row mb-3">
-    <h1 class="col-md-9"><?php echo $viewTitle ?></h1>
-    <form action="actions/save_notice.php" method="post">
-      <div id="input_test"></div>
+  <form action="actions/save_notice.php" method="post">
+    <div class="row mb-3">
+      <h1 class="col-md-9"><?php echo $viewTitle ?></h1>
       <!--Check for bulk importer-->
       <?php if ($hasBulkImporter) : ?>
         <div class="col-md-3 ">
@@ -96,101 +95,101 @@ if (!empty(get_object_vars($currentNotice))) {
       <?php
       endif;
       ?>
-  </div>
-  <!-- First row: Message, Status, Related URL -->
+    </div>
+    <!-- First row: Message, Status, Related URL -->
 
-  <div class="row mb-3">
-    <div class="col-md-5 mb-4">
-      <label class="col-form-label-lg font-weight-bold" for="message">Message</label>
-      <input type="text" class="form-control" id="message" name="message" placeholder="Message" value="<?php echo $message ?>">
-    </div>
-    <div class="col-md-3 ml-3 mb-4">
-      <label class="col-form-label-lg font-weight-bold" for="status">Status</label>
-      <select class="form-select custom-select mr-sm-2" id="status" name="status">
-        <?php
-        // Populate Status Dropdown
-        foreach ($statusArray as $status) :
-          echo '<option name="status" value=' . $status . '>' . $status . '</option>';
-        endforeach;
-        ?>
-      </select>
-    </div>
-    <div class="col-md-3 ml-3 mb-4">
-      <label class="col-form-label-lg font-weight-bold" for="related_url">Related URL</label>
-      <input type="url" class="form-control" id="related_url" name="related_url" aria-describedby="url_helper" placeholder="Related URL:" value="<?php echo $relatedURL ?>" required>
-    </div>
-    <div id="url_helper" class="form-text"></div>
-  </div>
-  <!-- Second row: Related Property, Meta Code Snippets, Meta More Info URL   -->
-  <div class="row">
-    <div class="col-md-3 mb-4">
-      <label class="col-form-label-lg font-weight-bold" for="property_id">Related Property</label>
-      <select class="form-select custom-select mr-sm-2" id="property_id" name="property_id" required>
-        <?php
-        // Populate Properties Array
-        foreach ($propertiesArray as $property)
-          echo '<option name="property_id" value=' . $property->id . '>' . $property->name . '</option>';
-        ?>
-      </select>
-    </div>
-    <div class="col-md-5 ml-md-3 mb-4">
-      <label class="col-form-label-lg font-weight-bold" for="meta_code_snippet[]">Meta:Code Snippet</label>
-      <div id="snippets" class="list-group mb-3">
-        <?php foreach ($snippetArray as $snippet) : ?>
-          <div class="position-relative">
-            <input class="list-group-item pr-5" name="meta_code_snippet[]" value="<?php echo $snippet ?>" />
-            <a onclick="deleteField()" class="position-absolute end-0 top-50 translate-middle-y">
-              <svg xmlns="http://www.w3.org/2000/svg" height="1.25em" viewBox="0 0 448 512">
-                <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
-              </svg>
-            </a>
-          </div>
+    <div class="row mb-3">
+      <div class="col-md-5 mb-4">
+        <label class="col-form-label-lg font-weight-bold" for="message">Message</label>
+        <input type="text" class="form-control" id="message" name="message" placeholder="Message" value="<?php echo $message ?>">
+      </div>
+      <div class="col-md-3 ml-3 mb-4">
+        <label class="col-form-label-lg font-weight-bold" for="status">Status</label>
+        <select class="form-select custom-select mr-sm-2" id="status" name="status">
           <?php
-        endforeach;
-        ?>
-        <input class="list-group-item pr-5" name="meta_code_snippet_new[]" value="<?php echo $snippet ?>" />
+          // Populate Status Dropdown
+          foreach ($statusArray as $status) :
+            echo '<option name="status" value=' . $status . '>' . $status . '</option>';
+          endforeach;
+          ?>
+        </select>
       </div>
-      <a onclick="addField()" class="btn btn-secondary">Add Snippet</button>
-
-      </a>
-      <?php if ($hasVisualizer) : ?>
-        <a href="add_report" class="btn btn-secondary">Visualizer</a>
-      <?php endif; ?>
+      <div class="col-md-3 ml-3 mb-4">
+        <label class="col-form-label-lg font-weight-bold" for="related_url">Related URL</label>
+        <input type="url" class="form-control" id="related_url" name="related_url" aria-describedby="url_helper" placeholder="Related URL:" value="<?php echo $relatedURL ?>" required>
+      </div>
+      <div id="url_helper" class="form-text"></div>
     </div>
-    <div class="col-md-3 ml-md-3 mb-4">
-      <label class="col-form-label-lg font-weight-bold" for="meta_more_info_url">Meta: More Info URL</label>
+    <!-- Second row: Related Property, Meta Code Snippets, Meta More Info URL   -->
+    <div class="row">
+      <div class="col-md-3 mb-4">
+        <label class="col-form-label-lg font-weight-bold" for="property_id">Related Property</label>
+        <select class="form-select custom-select mr-sm-2" id="property_id" name="property_id" required>
+          <?php
+          // Populate Properties Array
+          foreach ($propertiesArray as $property)
+            echo '<option name="property_id" value=' . $property->id . '>' . $property->name . '</option>';
+          ?>
+        </select>
+      </div>
+      <!-- <div id="input_test"></div> -->
+      <div class="col-md-5 ml-md-3 mb-4">
+        <label class="col-form-label-lg font-weight-bold" for="meta_code_snippet[]">Meta:Code Snippet</label>
+        <div id="snippets" class="list-group mb-3">
+          <?php foreach ($snippetArray as $snippet) : ?>
+            <div class="position-relative">
+              <input class="list-group-item pr-5" name="meta_code_snippet[]" value="<?php echo $snippet ?>" />
+              <a onclick="deleteField()" class="position-absolute end-0 top-50 translate-middle-y">
+                <svg xmlns="http://www.w3.org/2000/svg" height="1.25em" viewBox="0 0 448 512">
+                  <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
+                </svg>
+              </a>
+            </div>
+          <?php
+          endforeach;
+          ?>
+        </div>
+        <a onclick="addField()" class="btn btn-secondary">Add Snippet</button>
 
-      <input type="url" class="form-control" id="meta_more_info_url" name="meta_more_info_url" placeholder="More Info URL:" value="<?php echo $moreInfoURL ?>">
-    </div>
-  </div>
-  <!-- Third row:  Meta Notes, Achive Notice-->
-  <div class="row">
-    <div class="col-md-4 mb-4">
-      <label class="col-form-label-lg font-weight-bold" for="meta_notes">Meta: Notes</label>
+        </a>
+        <?php if ($hasVisualizer) : ?>
+          <a href="add_report" class="btn btn-secondary">Visualizer</a>
+        <?php endif; ?>
+      </div>
+      <div class="col-md-3 ml-md-3 mb-4">
+        <label class="col-form-label-lg font-weight-bold" for="meta_more_info_url">Meta: More Info URL</label>
 
-      <textarea type="text" class="form-control" id="meta_notes" name="meta_notes" placeholder="Notes:" aria-label="notes" value="<?php echo $notes ?>"><?php echo $notes ?></textarea>
-    </div>
-    <div class="col-md-4 mb-4">
-      <label class="col-form-label-lg font-weight-bold" for="archived">Archive Notice:</label>
-      <div class="form-check form-switch text-center ml-4">
-        <input class="form-check-input  ml-4" name="archived" type="checkbox" id="archived" aria-label="Toggle Button" aria-checked="true"<?php if($archived) echo 'checked'; ?>>
+        <input type="url" class="form-control" id="meta_more_info_url" name="meta_more_info_url" placeholder="More Info URL:" value="<?php echo $moreInfoURL ?>">
       </div>
     </div>
-  </div>
-  <!-- Fourth row: Save Notice, Delete Notice-->
+    <!-- Third row:  Meta Notes, Achive Notice-->
+    <div class="row">
+      <div class="col-md-4 mb-4">
+        <label class="col-form-label-lg font-weight-bold" for="meta_notes">Meta: Notes</label>
 
-  <div class="row">
-    <div class="col-md-4 mb-4">
-      <button type="submit" id="submit" class="btn btn-primary">
-        Save notice
-      </button>
-      <button type="submit" id="submit" class="btn btn-danger">
-        Delete Notice
-      </button>
+        <textarea type="text" class="form-control" id="meta_notes" name="meta_notes" placeholder="Notes:" aria-label="notes" value="<?php echo $notes ?>"><?php echo $notes ?></textarea>
+      </div>
+      <div class="col-md-4 mb-4">
+        <label class="col-form-label-lg font-weight-bold" for="archived">Archive Notice:</label>
+        <div class="form-check form-switch text-center ml-4">
+          <input class="form-check-input  ml-4" name="archived" type="checkbox" id="archived" aria-label="Toggle Button" aria-checked="true" <?php if ($archived) echo 'checked'; ?>>
+        </div>
+      </div>
     </div>
-    <!-- <div class="col-md-4 mb-4">
+    <!-- Fourth row: Save Notice, Delete Notice-->
+
+    <div class="row">
+      <div class="col-md-4 mb-4">
+        <button type="submit" id="submit" class="btn btn-primary">
+          Save notice
+        </button>
+        <button type="submit" id="submit" class="btn btn-danger">
+          Delete Notice
+        </button>
+      </div>
+      <!-- <div class="col-md-4 mb-4">
     </div> -->
-  </div>
+    </div>
   </form>
 </div>
 
@@ -198,8 +197,7 @@ if (!empty(get_object_vars($currentNotice))) {
 
 <script>
   function addField($snippet_text) {
-    let snippets = document.getElementById("input_test");
-    // let snippets = document.getElementById("snippets");
+    let snippets = document.getElementById("snippets");
     let newSnippet = document.createElement("div");
     newSnippet.className = "position-relative";
     // newSnippet.className = "position-relative";
