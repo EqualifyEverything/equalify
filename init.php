79e6e0cf23b6418838b49fb28ffbd9d27d144f9b
azdak <negatia@gmail.com>
2023-12-14 10:43:37 -0600

    Added basic auth routes

--- init.php
+++ init.php
@@ -12,7 +12,20 @@ require __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__, '.env');
 $dotenv->load();
 
-if (array_key_exists('MODE', $_ENV) &&  $_ENV['MODE'] == 'managed') { // if we're in managed mode, initialize auth0
+$managed_mode = false;
+if (array_key_exists('MODE', $_ENV) &&  $_ENV['MODE'] == 'managed') { 
+    $managed_mode = true;
+};
+
+if($managed_mode){ // if we're in managed mode, initialize auth0
+
+    define('ROUTE_URL_INDEX', rtrim($_ENV['AUTH0_BASE_URL'], '/'));
+    define('ROUTE_URL_LOGIN', ROUTE_URL_INDEX . '?view=login');
+    define('ROUTE_URL_CALLBACK', ROUTE_URL_INDEX . '?view=auth_callback');
+    define('ROUTE_URL_LOGOUT', ROUTE_URL_INDEX . '?view=logout');
+
+    echo ROUTE_URL_CALLBACK;
+
     $auth0 = new \Auth0\SDK\Auth0([
         'domain' => $_ENV['AUTH0_DOMAIN'],
         'clientId' => $_ENV['AUTH0_CLIENT_ID'],
@@ -20,12 +33,15 @@ if (array_key_exists('MODE', $_ENV) &&  $_ENV['MODE'] == 'managed') { // if we'r
         'cookieSecret' => $_ENV['AUTH0_COOKIE_SECRET']
     ]);
 
-     $session = $auth0->getCredentials();
+    $session = $auth0->getCredentials();
 
-    if ($session === null) {
-        // The user isn't logged in.
-        echo '<p>Please <a href="/login">log in</a>.</p>';
-        return;
-    } 
-
+    if ($session === null) {  // The user isn't logged in.      
+        echo '<p>Please <a href="/?view=login">log in</a>.</p>';
+    } else {
+        echo '<pre>';
+        print_r($session->user);
+        echo '</pre>';
+      
+        echo '<p>You can now <a href="/?view=logout">log out</a>.</p>';
+    }
 }
