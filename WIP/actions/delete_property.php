b4a39cbf95e107d2a8f914a6a19eeab7d9d98e08
Blake Bertuccelli-Booth <blakebertuccelli@gmail.com>
2024-01-11 11:23:35 -0600

    Fix redirect

--- WIP/actions/delete_property.php
+++ WIP/actions/delete_property.php
@@ -62,7 +62,7 @@ try {
     $pdo->commit();
 
     // Success redirection or message
-    header("Location: ../index.php?view=setting&success=Property and related data deleted.");
+    header("Location: ../index.php?view=settings&success=Property and related data deleted.");
     exit;
     
 } catch (Exception $e) {
