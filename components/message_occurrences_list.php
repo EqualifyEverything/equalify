c83b50d179f3b674511026e51ebe498bb310a460
Blake Bertuccelli-Booth <blakebertuccelli@gmail.com>
2024-01-12 11:00:23 -0600

    Tease AI Analysis

--- components/message_occurrences_list.php
+++ components/message_occurrences_list.php
@@ -9,7 +9,10 @@ function the_message_occurrences_list($filters = '')
     <div class="d-flex flex-column flex-md-row align-items-center">
         <h3>Occurrences</h3>
         <div class="ms-md-auto">
-            <a href="actions/rescan.php?<?php echo $filters;?>" class="btn btn-sm btn-primary">
+            <a href="#" class="btn btn-sm btn-outline-primary">
+                AI Analysis
+            </a>
+            <a href="actions/rescan.php?<?php echo $filters;?>" class="btn btn-sm btn-outline-primary">
                 Rescan All
             </a>
         </div>
