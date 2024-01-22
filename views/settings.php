b39745d774263b3d84935363e0b470a77dfc3a83
Blake Bertuccelli-Booth <blakebertuccelli@gmail.com>
2024-01-22 12:52:52 -0600

    Simplify verbage

--- views/settings.php
+++ views/settings.php
@@ -48,7 +48,7 @@ require_once('components/success_or_error_message.php');
                 if(!empty($property_scanned)){
                     $date_object = new DateTime($property_scanned);
                     $formatted_date = $date_object->format('m/d/y');
-                    echo '<small class="text-muted">Scanned on '.$formatted_date.'.</small>'; 
+                    echo '<small class="text-muted">Scanned '.$formatted_date.'.</small>'; 
                 }
                 ?>
             </div>
