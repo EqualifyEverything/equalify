5f74cc8829f1a0fa47c3f75a0cdfb6f55b7d725b
Blake Bertuccelli-Booth <blakebertuccelli@gmail.com>
2024-01-30 10:19:16 -0600

    Clean up Report header linking

--- components/report_header.php
+++ components/report_header.php
@@ -46,14 +46,38 @@ function the_report_header() {
 
         <div class="d-flex flex-column flex-md-row align-items-center mt-4">
             <h1 class="display-5" style="max-width:800px">
-                <a href="?view=report&report_id=<?php echo $report_id;?>" class="link-dark link-underline link-underline-opacity-0 link-underline-opacity-75-hover">
 
-                    <?php
+                <?php
+                // Link sub report pages to main report
+                if($_GET['view'] == 'report'){
+                
                     // Page Title
                     echo get_title($report_id, 'report');
                     ?>
 
                 </a>
+
+                <?php
+                // Link sub report pages to main report
+                }else{
+                ?>
+
+                <a href="?view=report&report_id=<?php echo $report_id;?>" class="link-dark link-underline link-underline-opacity-0 link-underline-opacity-75-hover">
+
+                <?php
+                // Page Title
+                echo get_title($report_id, 'report');
+                ?>
+
+                    <span class="visually-hidden"> (Linked to Main Report)</span>
+                </a>
+
+                <?php
+                // Link sub report pages to main report
+                };
+                ?>
+
+
             </h1>
             <div class="ms-md-auto">
                 <a class="btn btn-secondary" href="?view=report_settings&report_id=<?php echo $report_id;?>">
