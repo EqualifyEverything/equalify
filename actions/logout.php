96479ff55dc2f1155790fc0e2b6489c62165e8ce
Blake Bertuccelli-Booth <blakebertuccelli@gmail.com>
2024-02-07 15:33:47 -0600

    rescanning

--- actions/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-    // Clear the user's local session with our app, then redirect them to the Auth0 logout endpoint to clear their Auth0 session.
-    header("Location: " . $auth0->logout(ROUTE_URL_INDEX));
-    exit;
-?>
--- helpers/get_scans_count.php
+++
@@ -1,9 +0,0 @@
-<?php
-function get_scans_count(){
-    global $pdo;
-
-    // Query to get total count of scans
-    $sql = "SELECT COUNT(*) FROM queued_scans";
-    $stmt = $pdo->query($sql);
-    return $stmt->fetchColumn();
-}
