3ec7d351ef95c7df78e9a3627b94ae1fe4d6ea5d
azdak <negatia@gmail.com>
2025-08-27 17:34:42 -0500

    Trigger lambda deploy

--- services/aws-lambda-triage/src/lambda.ts
+++ services/aws-lambda-triage/src/lambda.ts
@@ -7,7 +7,7 @@ export async function handler(
   event: LambdaFunctionUrlEvent,
   context: Context,
 ): Promise<LambdaFunctionUrlResult> {
-  console.log("beep boop ...")
+  console.log("beep boop")
   console.log(context.functionName)
   console.log(`${event.requestContext.http.method} ${event.rawPath}`)
   return {
