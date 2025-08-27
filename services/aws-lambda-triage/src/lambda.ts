8cfa0d8da4960dbfbd40a2f8f76b654b8ac8ab7f
azdak <negatia@gmail.com>
2025-08-27 15:49:03 -0500

    Lambda scaffold

---
+++ services/aws-lambda-triage/src/lambda.ts
@@ -0,0 +1,17 @@
+import type { APIGatewayProxyEventV2, APIGatewayProxyResultV2, Context } from 'aws-lambda'
+
+type LambdaFunctionUrlEvent = APIGatewayProxyEventV2;
+type LambdaFunctionUrlResult = APIGatewayProxyResultV2;
+
+export async function handler(
+  event: LambdaFunctionUrlEvent,
+  context: Context,
+): Promise<LambdaFunctionUrlResult> {
+  console.log(context.functionName)
+  console.log(`${event.requestContext.http.method} ${event.rawPath}`)
+  return {
+    statusCode: 200, 
+    headers: { 'content-type': 'application/json' },
+    body: JSON.stringify(event, null, 2),
+  }
+}
