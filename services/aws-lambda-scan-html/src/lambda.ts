4c85e8c2241e8b13787979cab7d48819d568b43e
azdak <negatia@gmail.com>
2025-09-04 09:53:13 -0500

    Chromium integration: log tweaks

--- services/aws-lambda-scan-html/src/lambda.ts
+++ services/aws-lambda-scan-html/src/lambda.ts
@@ -9,7 +9,7 @@ import { PartialItemFailureResponse } from "@aws-lambda-powertools/batch/types";
 import { logMetrics } from "@aws-lambda-powertools/metrics/middleware";
 import { MetricUnit } from "@aws-lambda-powertools/metrics";
 
-import {logger, metrics} from './telemetry'
+import { logger, metrics } from "./telemetry";
 import scan from "./scan";
 
 const processor = new BatchProcessor(EventType.SQS);
@@ -19,11 +19,12 @@ const recordHandler = async (record: SQSRecord): Promise<void> => {
   metrics.captureColdStartMetric();
   const startTime = performance.now();
   const payload = record.body;
+
+  const payloadParsed = JSON.parse(payload);
+  const job = JSON.parse(payloadParsed);
   if (payload) {
     try {
-      const payloadParsed = JSON.parse(payload);
-      const job = JSON.parse(payloadParsed);
-      logger.info("Processing ", job);
+      logger.info("Processing ", job.url);
       metrics.addMetric("scansStarted", MetricUnit.Count, 1);
       const results = await scan(job);
       logger.info(JSON.stringify(results));
@@ -33,7 +34,7 @@ const recordHandler = async (record: SQSRecord): Promise<void> => {
     } finally {
       const endTime = performance.now(); // End timing
       const executionDuration = endTime - startTime; // Calculate duration in milliseconds
-
+      logger.info("Finished", job.url);
       // Add a custom metric for execution duration
       metrics.addMetric(
         "ScanDuration",
