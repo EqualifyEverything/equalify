a7bcbacb1a1d283fbfbe0780fd69dd2a4f98ee63
azdak <negatia@gmail.com>
2025-09-17 14:42:42 -0500

    Updated PDF scan vera call path for layer

--- services/aws-lambda-scan-pdf/src/scan.ts
+++ services/aws-lambda-scan-pdf/src/scan.ts
@@ -44,7 +44,7 @@ export default async function (job: SqsScanJob) {
       ? ""
       : "--nonpdfext ";
     const stdout = await execRun(
-      "~/verapdf -f ua2 --format json " + nonpdfextFlag + filePath
+      "~/opt/vera/verapdf -f ua2 --format json " + nonpdfextFlag + filePath
     );
     let report = stdout as string;
     veraPdfReport = JSON.parse(report);
