a35367a55b4c2baed3a3c71fde5ef1cb240732ac
azdak <negatia@gmail.com>
2025-09-18 18:04:20 -0500

    Fixed wrong log output in pdf scan

--- services/aws-lambda-scan-pdf/src/telemetry.ts
+++ services/aws-lambda-scan-pdf/src/telemetry.ts
@@ -3,6 +3,6 @@ import { Metrics } from "@aws-lambda-powertools/metrics";
 
 export const metrics = new Metrics({
   namespace: "equalifyuic",
-  serviceName: "aws-lambda-scan-html",
+  serviceName: "aws-lambda-scan-pdf",
 });
-export const logger = new Logger({ serviceName: "aws-lambda-scan-html" });
+export const logger = new Logger({ serviceName: "aws-lambda-scan-pdf" });
