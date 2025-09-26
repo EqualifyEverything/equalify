07fe4aebea325fb00f1fa90093700948aa7d064f
azdak <negatia@gmail.com>
2025-09-26 18:42:43 -0500

    verapdf-interface: refactored to use native java vera

--- services/aws-lambda-verapdf-interface/src/main/java/com/equalifyuic/app/handler.java
+++ services/aws-lambda-verapdf-interface/src/main/java/com/equalifyuic/app/handler.java
@@ -2,6 +2,7 @@ package com.equalifyuic.app;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
@@ -14,6 +15,17 @@ import org.apache.commons.io.FileUtils;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import org.verapdf.core.EncryptedPdfException;
+import org.verapdf.core.ModelParsingException;
+import org.verapdf.core.ValidationException;
+import org.verapdf.gf.foundry.VeraGreenfieldFoundryProvider;
+import org.verapdf.pdfa.Foundries;
+import org.verapdf.pdfa.PDFAParser;
+import org.verapdf.pdfa.results.ValidationResult;
+import org.verapdf.processor.reports.multithread.writer.JsonReportWriter;
+import org.verapdf.pdfa.PDFAValidator;
+import org.verapdf.pdfa.flavours.PDFAFlavour;
 
 public class handler implements RequestHandler<String, String> {
     @Override
@@ -50,8 +62,24 @@ public class handler implements RequestHandler<String, String> {
             logger.log(e.getMessage());
         }
 
+        VeraGreenfieldFoundryProvider.initialise();
+        String output = "";
+        try (PDFAParser parser = Foundries.defaultInstance().createParser(new FileInputStream(filePath))) {
+            PDFAValidator validator = Foundries.defaultInstance().createValidator(parser.getFlavour(), false);
+            ValidationResult result = validator.validate(parser);
+            output = result.getFailedChecks().toString();
+            /* if (result.isCompliant()) {
+                // File is a valid PDF/A 1b
+            } else {
+                // it isn't
+            } */
+        } catch (IOException | ValidationException | ModelParsingException | EncryptedPdfException exception) {
+            // Exception during validation
+            logger.log(exception.toString());
+        }
+
         // if the file doesn't end in PDF, pass the --nonpdfext flag
-        String nonpdfextFlag = filePath.toString().endsWith(".pdf")
+        /* String nonpdfextFlag = filePath.toString().endsWith(".pdf")
                 ? ""
                 : "--nonpdfext ";
         ProcessBuilder pb = new ProcessBuilder(
@@ -85,7 +113,7 @@ public class handler implements RequestHandler<String, String> {
 
         } catch (IOException | InterruptedException e) {
             logger.log(e.toString());
-        }
+        } */
 
         logger.log("Processing complete for " + input);
         logger.log(output);
