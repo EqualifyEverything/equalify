d8ae2006b8816cab2cd6ee3e48c7b835f0ab01e5
azdak <negatia@gmail.com>
2025-09-26 15:51:17 -0500

    verapdf-interface: more debug

--- services/aws-lambda-verapdf-interface/src/main/java/com/equalifyuic/app/handler.java
+++ services/aws-lambda-verapdf-interface/src/main/java/com/equalifyuic/app/handler.java
@@ -15,7 +15,7 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class main implements RequestHandler<String, String> {
+public class handler implements RequestHandler<String, String> {
     @Override
     public String handleRequest(String input, Context context) {
         LambdaLogger logger = context.getLogger();
