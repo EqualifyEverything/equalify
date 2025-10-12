07d07a0c80a675e5024c13793af6ae80c3c42643
azdak <negatia@gmail.com>
2025-10-12 12:48:22 -0500

    PDF-scan: Errors now still return results to endpoint

--- services/aws-lambda-scan-pdf/src/lambda.ts
+++ services/aws-lambda-scan-pdf/src/lambda.ts
@@ -8,7 +8,9 @@ import {
 import type { PartialItemFailureResponse } from "@aws-lambda-powertools/batch/types";
 import { logMetrics } from "@aws-lambda-powertools/metrics/middleware";
 import { MetricUnit } from "@aws-lambda-powertools/metrics";
-import convertVeraToEqualifyV2, { ReportData } from "../../../shared/convertors/VeraToEqualify2.ts"
+import convertVeraToEqualifyV2, {
+  ReportData,
+} from "../../../shared/convertors/VeraToEqualify2.ts";
 
 import { logger, metrics } from "./telemetry.ts";
 import scan from "./scan.ts";
@@ -25,7 +27,7 @@ const recordHandler = async (record: SQSRecord): Promise<void> => {
 
   const payloadParsed = JSON.parse(payload);
   const job = JSON.parse(payloadParsed);
-  logger.info("PDF to scan payload:", job)
+  logger.info("PDF to scan payload:", job);
   if (payload) {
     try {
       metrics.addMetric("scansStarted", MetricUnit.Count, 1);
@@ -42,11 +44,13 @@ const recordHandler = async (record: SQSRecord): Promise<void> => {
       if (results) {
         logger.info(results);
         const parsedResult = JSON.parse(results);
-        const equalifiedResults = convertVeraToEqualifyV2(parsedResult as ReportData, job)
+        const equalifiedResults = convertVeraToEqualifyV2(
+          parsedResult as ReportData,
+          job
+        );
         logger.info(`Job [${job.id}] Scan Complete!`);
-        
-        logger.info("Sending to API...", 
-          JSON.stringify(equalifiedResults));
+
+        logger.info("Sending to API...", JSON.stringify(equalifiedResults));
 
         const sendResultsResponse = await fetch(RESULTS_ENDPOINT, {
           method: "post",
@@ -58,17 +62,35 @@ const recordHandler = async (record: SQSRecord): Promise<void> => {
           "PDF-scan Results sent to API!",
           JSON.stringify(sendResultsResponse.json())
         );
-        
       } else {
         logger.error("Error:", results);
+        await sendFailedStatusToResultsEndpoint(job);
       }
     } catch (error) {
       logger.error("Scan Error!", error as string);
+      await sendFailedStatusToResultsEndpoint(job);
       throw error;
     }
   }
   metrics.publishStoredMetrics();
   return;
+};
+
+const sendFailedStatusToResultsEndpoint = async (job: any) => {
+  const failurePayload = {
+    auditId: job.auditId,
+    urlId: job.urlId,
+    status: "failed",
+    error: "PDF Scan encountered an error.",
+    blockers: [],
+  };
+
+  const sendResultsResponse = await fetch(RESULTS_ENDPOINT, {
+    method: "post",
+    body: JSON.stringify(failurePayload),
+    headers: { "Content-Type": "application/json" },
+  });
+  return sendResultsResponse;
 };
 
 // handle batch
