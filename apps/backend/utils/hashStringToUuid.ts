2f4f12c6638fde7de77280c77dc8deedd8150d6a
Christopher Aitken <chris@heythisischris.com>
2025-10-29 16:47:27 -0400

    Switched content hash from single MD5 to double SHA-256 (inspired by blockchain)

--- apps/backend/utils/hashStringToUuid.ts
+++ apps/backend/utils/hashStringToUuid.ts
@@ -1,5 +1,7 @@
 import { createHash } from 'crypto';
 
 export const hashStringToUuid = (input) => {
-    return createHash('md5').update(input).digest('hex');
+    const hash1 = createHash('sha256').update(input).digest();
+    const hash2 = createHash('sha256').update(hash1).digest('hex');
+    return hash2.slice(0, 32);
 }
