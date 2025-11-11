5d87d4e6ab847bf90dc1a7ddbd4fbc80bfbf00b7
azdak <negatia@gmail.com>
2025-11-10 20:17:49 -0600

    Logs: typo in GraphQL query

--- apps/backend/routes/auth/getLogs.ts
+++ apps/backend/routes/auth/getLogs.ts
@@ -24,7 +24,7 @@ export const getLogs = async () => {
                 }
                 logs_aggregate: {
                     aggregate: {
-                        count: 71
+                        count
                     }
                 }
             }`,
