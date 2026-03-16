9314b8d09f76f456b03245f12df3dd97fc352a8f
Christopher Aitken <chris@heythisischris.com>
2026-03-16 16:12:22 -0400

    Fixed bug w/ undefined headers, now allowing graphqlQuery to run via EventBrdige

--- apps/backend/utils/graphqlQuery.ts
+++ apps/backend/utils/graphqlQuery.ts
@@ -2,7 +2,7 @@ import { event, isStaging } from '.';
 
 export const graphqlQuery = async ({ query, variables = {} }) => {
     const authorization = event?.headers?.authorization;
-    const role = event?.headers['x-hasura-role'];
+    const role = event?.headers?.['x-hasura-role'];
     const headers = {
         'Content-Type': 'application/json',
         ...authorization ? { authorization } : { 'x-hasura-admin-secret': process.env.DB_PASSWORD },
