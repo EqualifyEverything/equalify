da1235e37a208f16ec421229ef03f0dbd90bf183
Christopher Aitken <chris@heythisischris.com>
2026-05-18 13:01:41 -0400

    feat(shortId): Increased short ID length from 6 to 8 characters

--- apps/backend/utils/generateShortId.ts
+++ apps/backend/utils/generateShortId.ts
@@ -1,7 +1,7 @@
 import { customAlphabet } from 'nanoid';
 
-const letters = customAlphabet('ABCDEFGHIJKLMNOPQRSTUVWXYZ', 3);
-const numbers = customAlphabet('0123456789', 3);
+const letters = customAlphabet('ABCDEFGHIJKLMNOPQRSTUVWXYZ', 4);
+const numbers = customAlphabet('0123456789', 4);
 
 export const generateShortId = (): string => {
     return `${letters()}${numbers()}`;
--- apps/backend/utils/validateShortId.ts
+++ apps/backend/utils/validateShortId.ts
@@ -1,4 +1,4 @@
 export const validateShortId = (id: string): boolean => {
-  const shortIdRegex = /^[A-Z]{3}[0-9]{3}$/;
+  const shortIdRegex = /^([A-Z]{3}[0-9]{3}|[A-Z]{4}[0-9]{4})$/;
   return shortIdRegex.test(id);
 };
