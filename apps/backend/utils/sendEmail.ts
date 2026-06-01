c865f7926fe4814e851745d0ec57b984142b65c6
azdak <negatia@gmail.com>
2026-06-01 10:49:11 -0500

    refactor(sendEmail): Removing hardcoded values

--- apps/backend/utils/sendEmail.ts
+++ apps/backend/utils/sendEmail.ts
@@ -1,12 +1,12 @@
 import { SES } from '@aws-sdk/client-ses';
 import { formatEmail } from './formatEmail';
-const ses = new SES({ region: 'us-east-2' });
+const ses = new SES({ region: process.env.AWS_REGION ?? "us-east-2" });
 
 export const sendEmail = async ({ to, subject, body }) => {
     try {
         await ses.sendEmail({
             Destination: { ToAddresses: [to] },
-            Source: `noreply@equalify.uic.edu`,
+            Source: process.env.SES_ADMIN_EMAIL ?? `noreply@equalify.uic.edu`,
             Message: {
                 Subject: { Data: subject },
                 Body: { Html: { Data: formatEmail({ body }) } },
